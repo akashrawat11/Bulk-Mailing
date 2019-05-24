d88c4edf35818cd81b901bd158fd1521ac690f81
akashrawat11 <ar053731@gmail.com>
2019-05-24 17:21:57 +0530

    Update mailing.py

--- mailing.py
+++ mailing.py
@@ -7,12 +7,11 @@ from email import encoders
 
 with open('example.csv') as csvfile:
     readCSV = csv.reader(csvfile, delimiter = ',')
-
-
     server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
     server.login('Senders E-mail','password')
-    try:
-        server.sendmail('Senders E-mail','recievers E-mail','Subject: {}\n\n Message <python>'.format("Subject"))
-    except:
-        print("not able to send")
+    for row in readCSV:
+        try:
+            server.sendmail('Senders E-mail',row[0],'Subject: {}\n\n Message <python>'.format("Subject"))
+        except:
+            print("not able to send")
 server.quit()
